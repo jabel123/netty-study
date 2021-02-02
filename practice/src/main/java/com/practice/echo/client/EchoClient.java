fda5e25b50d2534d829ce97a22e0a771fb051228
DESKTOP-RRSAESK\jabel <jabel123@naver.com>
2021-02-03 08:53:18 +0900

    practice(#12) : 아침 연습
    이전에 EchoServer, EchoClient 만들었던것 기억 되살려서 만들어 보기

---
+++ practice/src/main/java/com/practice/echo/client/EchoClient.java
@@ -0,0 +1,44 @@
+package com.practice.echo.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoClient {
+    public static void main(String[] args) {
+        try {
+            new EchoClient().start();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void start() throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress("localhost", 5001))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+
+    }
+}
